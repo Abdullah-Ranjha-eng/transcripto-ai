bead84b3592bbc04d4c1740b4c8a2df05d9ee737
Abdullah Ranjha <ahmedilyas5440@gmail.com>
2026-07-16 21:48:14 +0500

    Updated axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
-  withCredentials: true,   // sends the JWT cookie automatically
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1` || "http://localhost:5000/api/v1",
+  withCredentials: true,
 });
 
 export default api;
